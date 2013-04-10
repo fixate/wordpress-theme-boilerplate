fcfb63d2a82e374aba8895e3459beed328ed2c85
Larry Botha <larrywp@themousepotatowebsite.co.za>
2013-04-10 17:24:30 +0200

    update theme name for localisation

--- category.php
+++ category.php
@@ -14,7 +14,7 @@
 get_header(); ?>
 
 <?php if ( have_posts()) : ?>
-	<h1><?php printf( __('Category Archives: %s', 'twentytwelve'), '<span>' . single_cat_title('', false) . '</span>'); ?></h1>
+	<h1><?php printf( __('Category Archives: %s', 'theme_name'), '<span>' . single_cat_title('', false) . '</span>'); ?></h1>
 
 	<?php if ( category_description()) : // Show an optional category description ?>
 		<div><?php echo category_description(); ?></div>
