4f182887fc637a52a9cec30907aac889ceaa349d
Larry Botha <larrywp@themousepotatowebsite.co.za>
2013-04-26 11:39:54 +0200

    ensure wp-config doesn't block error messages in local environment

--- !root/wp-config.php
+++ !root/wp-config.php
@@ -77,7 +77,11 @@ define('WPLANG', '');
 // Hide errors
  * ===========================================================================*/
 ini_set('display_errors', 0);
-define('WP_DEBUG_DISPLAY', false);
+if (file_exists(dirname(__FILE__) . '/local-config.php')) {
+	define('WP_DEBUG_DISPLAY', true);
+} else {
+	define('WP_DEBUG_DISPLAY', false);
+}
 
 /* ============================================================================
  * Debug mode
