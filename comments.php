abd41819fbafbf243943ac1fa3c558a33771d408
Larry Botha <larrywp@themousepotatowebsite.co.za>
2013-05-16 16:18:54 +0200

    clean up layout slightly

--- comments.php
+++ comments.php
@@ -22,18 +22,22 @@ if ( post_password_required())
 ?>
 
 <?php if ( comments_open() || have_comments()) : ?>
+
 <section id="comments">
 
-	<h2><?php printf( _n('One response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'theme_name'),
-					number_format_i18n( get_comments_number()), '<span>' . get_the_title() . '</span>'); ?></h2>
+	<h2>
+		<?php printf( _n('One response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'bspark'), number_format_i18n(get_comments_number()), '<span>' . get_the_title() . '</span>'); ?>
+	</h2>
+
 	<?php if ( have_comments()) : ?>
 
-		<ol>
-			<?php wp_list_comments( array('callback' => 'theme_name_comment', 'style' => 'ol')); ?>
+		<ol class="list-reset">
+			<?php wp_list_comments( array('callback' => 'bspark_comment', 'style' => 'ol')); ?>
 		</ol><!-- .commentlist -->
 
 	<?php else : ?>
-		<p><?php _e('Be the first to leave a comment!', 'theme_name')?></p>
+
+		<p><?php _e('Be the first to leave a comment!', 'bspark')?></p>
 
 	<?php endif; // have_comments() ?>
 
@@ -41,28 +45,28 @@ if ( post_password_required())
 	$custom_comment_form = array(
 		'fields' => apply_filters('comment_form_default_fields', array(
 			'author' => '<p class="comment-form-author">' .
-				'<label for="author">' . __('Your Name' , 'theme_name') . '</label> ' .
+				'<label for="author">' . __('Your Name' , 'bspark') . '</label> ' .
 				($req ? '<span class="required">*</span>' : '') .
 				'<input id="author" name="author" type="text" value="' .
 				esc_attr($commenter['comment_author']) . '" size="30"' . ' class="required" />' .
 				'</p>',
 			'email'  => '<p class="comment-form-email">' .
-				'<label for="email">' . __('Your Email' , 'theme_name') . '</label> ' .
+				'<label for="email">' . __('Your Email' , 'bspark') . '</label> ' .
 				($req ? '<span class="required">*</span>' : '') .
 				'<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email']) . '" size="30"' . ' class="required email" />' .
 				'</p>',
 			'url'    =>  '<p class="comment-form-url">' .
-				'<label for="website">' . __('Your Website' , 'theme_name') . '</label> ' . '<input id="url" name="url" type="text" value="' . esc_attr(  $commenter['comment_author_url']) . '" size="30"' . ' />' .
+				'<label for="website">' . __('Your Website' , 'bspark') . '</label> ' . '<input id="url" name="url" type="text" value="' . esc_attr(  $commenter['comment_author_url']) . '" size="30"' . ' />' .
 				'</p>')),
 			'comment_field' => '<p class="comment-form-comment">' .
 				'<textarea id="comment" name="comment" cols="45" rows="8" aria-required="true" class="required"></textarea>' .
 				'</p>',
 			'logged_in_as' => '<p class="logged-in-as">' . sprintf( __('Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s">Log out?</a>'), admin_url('profile.php'), $user_identity, wp_logout_url( apply_filters('the_permalink', get_permalink($post->id)))) . '</p>',
-			'title_reply' => __('Leave a Reply' , 'theme_name'),
+			'title_reply' => __('Leave a Reply' , 'bspark'),
 			'comment_notes_before' => '',
 			'comment_notes_after' => '',
-			'cancel_reply_link' => __('Cancel' , 'theme_name'),
-			'label_submit' => __('Post Comment' , 'theme_name'),
+			'cancel_reply_link' => __('Cancel' , 'bspark'),
+			'label_submit' => __('Post Comment' , 'bspark'),
 		);
 		comment_form($custom_comment_form);
 	?>
