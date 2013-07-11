609cef5cc29265f40d9467fbc733cf73781308e9
Larry Botha <larry@fixate.it>
2013-07-11 10:56:13 +0200

    move head to its own partial,
    fix menu fallback function name
    improve navigation class names

--- header.php
+++ header.php
@@ -13,41 +13,9 @@
 <!--[if IE 8]>    <html class="no-js ie8 oldie" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 9]>    <html class="no-js ie9 oldie" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 9]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<link rel="dns-prefetch" href="//ajax.googleapis.com" />
 
-		<title><?php wp_title('|'); ?></title>
+	<?php get_template_part('partials/meta') ?>
 
-		<?php // enable responsive behaviour for all devices ?>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<?php // Windows 8 start screen tile ?>
-		<meta name="msapplication-TileColor" content="#ffffff"/>
-		<meta name="msapplication-TileImage" content="apple-touch-icon-144x144-precomposed.png"/>
-
-		<link rel="SHORTCUT ICON" href="<?php echo THEME_URI; ?>/img/favicon.ico" type="image/x-icon" />
-		<link rel="logo" type="image/svg" href="<?php echo THEME_URI; ?>/img/logo.svg"/>
-		<!--[if ! lte IE 7]><!-->
-		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
-		<!--<![endif]-->
-
-		<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-		<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
-		<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-		<!--[if lte IE 7]>
-			<link rel="stylesheet" href="http://universal-ie6-css.googlecode.com/files/ie6.1.1.css" media="screen, projection">
-		<![endif]-->
-
-		<!--[if lt IE 9]>
-		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
-
-		<?php wp_head(); ?>
-	</head>
-	<?php flush(); ?>
 	<body <?php body_class(); ?>>
 
 		<!--[if lte IE 8]>
@@ -68,7 +36,7 @@
 
 			<nav id="nav" role="navigation">
 				<?php wp_nav_menu(array(
-					'theme_location' => 'primary-menu',
+					'theme_location' => 'menu-primary',
 					'fallback_cb' => 'default_primary_menu',
 					'menu_class' => 'menu',
 					'container' => false,
---
+++ partials/meta.php
@@ -0,0 +1,35 @@
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<link rel="dns-prefetch" href="//ajax.googleapis.com" />
+
+	<title><?php wp_title('|'); ?></title>
+
+	<?php // enable responsive behaviour for all devices ?>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<?php // Windows 8 start screen tile ?>
+	<meta name="msapplication-TileColor" content="#ffffff"/>
+	<meta name="msapplication-TileImage" content="apple-touch-icon-144x144-precomposed.png"/>
+
+	<link rel="SHORTCUT ICON" href="<?php echo THEME_URI; ?>/img/favicon.ico" type="image/x-icon" />
+	<link rel="logo" type="image/svg" href="<?php echo THEME_URI; ?>/img/logo.svg"/>
+	<!--[if ! lte IE 7]><!-->
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+	<!--<![endif]-->
+
+	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
+	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+	<!--[if lte IE 7]>
+		<link rel="stylesheet" href="http://universal-ie6-css.googlecode.com/files/ie6.1.1.css" media="screen, projection">
+	<![endif]-->
+
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<?php wp_head(); ?>
+</head>
+<?php flush(); ?>
