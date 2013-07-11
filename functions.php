bb9e34045715d703623933e95b6369e1a560e0ee
Larry Botha <larry@mailinator.com>
2013-07-11 10:56:13 +0200

    move head to its own partial,
    fix menu fallback function name
    improve navigation class names

--- functions.php
+++ functions.php
@@ -138,8 +138,8 @@ remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
  * @since Theme_Name 1.0
  */
 if ( ! function_exists('default_primary_menu')) {
-	function default_primary_nav() {
-		echo '<ul id="primary-menu" class="primary-menu">';
+	function default_primary_menu() {
+		echo '<ul class="menu-primary">';
 		wp_list_pages('title_li=');
 		echo '</ul>';
 	}
