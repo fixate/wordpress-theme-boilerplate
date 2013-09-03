42368b97297c0dd32b4e17f8cb8e44f7f6978b5a
Larry Botha <larry@fixate.it>
2013-09-03 23:39:01 +0200

    minor tweaks to layout

--- inc/widgets.php
+++ inc/widgets.php
@@ -1,5 +1,5 @@
-<?php 
+<?php
 
 // Include and register widgets
-// include_once 'widgets/foo.php'
-// add_action('widgets_init', create_function('', 'register_widget( "foo_widget");'));
+// include_once 'widgets/foo.php';
+// add_action('widgets_init', create_function('', 'register_widget("foo_widget");'));
