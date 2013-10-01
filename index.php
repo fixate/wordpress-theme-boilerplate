0828720aa4911f04038a9711ccee0a3976a2f52e
Larry Botha <larry@fixate.it>
2013-10-01 13:58:03 +0200

    ensure templates use post type and not format

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ get_header(); ?>
 <?php if (have_posts()) : ?>
 
 	<?php while (have_posts()) : the_post(); ?>
-		<?php get_template_part('content', get_post_format()); ?>
+		<?php get_template_part('content', get_post_type()); ?>
 	<?php endwhile; ?>
 
 	<?php get_template_part('partials/pagination'); ?>
